0aeae849d6e128d236d784daf8273c35c44431cc
Gerson <gersonrodriguessantos8@gmail.com>
2025-03-16 19:52:31 -0300

    fix: argocd provider version

--- terraform.tf
+++ terraform.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     argocd = {
       source  = "argoproj-labs/argocd"
-      version = ">= 7.5.0"
+      version = ">= 7"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
