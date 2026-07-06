b20168b2d705ac2f63993ad1e33fae487a314fc4
GersonRS <gersonrodriguessantos8@gmail.com>
2026-07-06 17:00:53 -0300

    fix: remove kiali and decouple gateway flow from lb ip

--- kind/local.tf
+++ kind/local.tf
@@ -3,7 +3,6 @@ locals {
     istiod = {}
   }]
 
-  gateway_ip      = try(data.kubernetes_service.istio_gateway.status[0].load_balancer[0].ingress[0].ip, "127.0.0.1")
-  gateway_domain  = "${trimprefix(var.subdomain, ".")}.${replace(local.gateway_ip, ".", "-")}.nip.io"
+  gateway_domain  = var.base_domain != "" ? "${var.subdomain != "" ? "${trimprefix(var.subdomain, ".")}." : ""}${var.base_domain}" : "${var.subdomain != "" ? trimprefix(var.subdomain, ".") : "apps"}.nip.io"
   kubectl_context = var.kubectl_context != "" ? var.kubectl_context : "kind-${var.cluster_name}"
 }
