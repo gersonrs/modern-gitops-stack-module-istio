ee6912eb8af185be3fbaf925253012f2bd92def8
GersonRS <gersonrodriguessantos8@gmail.com>
2026-07-06 17:56:13 -0300

    feat: replace local-exec crd/cert with declarative argocd app and kubernetes manifest

--- kind/local.tf
+++ kind/local.tf
@@ -3,6 +3,7 @@ locals {
     istiod = {}
   }]
 
-  gateway_domain  = var.base_domain != "" ? "${var.subdomain != "" ? "${trimprefix(var.subdomain, ".")}." : ""}${var.base_domain}" : "${var.subdomain != "" ? trimprefix(var.subdomain, ".") : "apps"}.nip.io"
+  gateway_ip      = try(data.kubernetes_service.istio_gateway.status[0].load_balancer[0].ingress[0].ip, "127.0.0.1")
+  gateway_domain  = var.base_domain != "" ? "${var.subdomain != "" ? "${trimprefix(var.subdomain, ".")}." : ""}${var.base_domain}" : format("%s.nip.io", replace(local.gateway_ip, ".", "-"))
   kubectl_context = var.kubectl_context != "" ? var.kubectl_context : "kind-${var.cluster_name}"
 }
