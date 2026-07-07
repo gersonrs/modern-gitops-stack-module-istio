aa535a6d00d729e80a139dd1efd4dfdf20dac930
GersonRS <gersonrodriguessantos8@gmail.com>
2026-07-07 11:39:18 -0300

    feat: try again

--- kind/local.tf
+++ kind/local.tf
@@ -3,6 +3,6 @@ locals {
     istiod = {}
   }]
 
-  gateway_ip     = resource.kubernetes_manifest.wait_for_istio_gateway.object.status.loadBalancer.ingress[0].ip
+  gateway_ip     = resource.kubernetes_manifest.wait_for_istio_gateway.object.status[0].loadBalancer[0].ingress[0].ip
   gateway_domain = format("%s.nip.io", replace(local.gateway_ip, ".", "-"))
 }
