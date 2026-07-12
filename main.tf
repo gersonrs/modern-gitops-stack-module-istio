f465ca8fde6268fa81ca6483a5e7885eb9eab950
GersonRS <gersonrodriguessantos8@gmail.com>
2026-07-12 20:51:42 -0300

    feat: add PodMonitor resources for istiod, ztunnel and gateway

--- main.tf
+++ main.tf
@@ -428,6 +428,68 @@ resource "argocd_application" "gateway" {
   ]
 }
 
+resource "argocd_application" "istio_monitoring" {
+  count = var.enable_service_monitor ? 1 : 0
+
+  metadata {
+    name      = var.destination_cluster != "in-cluster" ? "istio-monitoring-${var.destination_cluster}" : "istio-monitoring"
+    namespace = var.argocd_namespace
+    labels = merge({
+      "application" = "istio-monitoring"
+      "cluster"     = var.destination_cluster
+    }, var.argocd_labels)
+  }
+
+  timeouts {
+    create = "15m"
+    delete = "15m"
+  }
+
+  wait = var.app_autosync == { "allow_empty" = tobool(null), "prune" = tobool(null), "self_heal" = tobool(null) } ? false : true
+
+  spec {
+    project = var.argocd_project == null ? argocd_project.this[0].metadata.0.name : var.argocd_project
+
+    source {
+      repo_url        = var.project_source_repo
+      path            = "charts/istio-monitoring"
+      target_revision = var.target_revision
+    }
+
+    destination {
+      name      = var.destination_cluster
+      namespace = var.namespace
+    }
+
+    sync_policy {
+      dynamic "automated" {
+        for_each = toset(var.app_autosync == { "allow_empty" = tobool(null), "prune" = tobool(null), "self_heal" = tobool(null) } ? [] : [var.app_autosync])
+        content {
+          prune       = automated.value.prune
+          self_heal   = automated.value.self_heal
+          allow_empty = automated.value.allow_empty
+        }
+      }
+
+      retry {
+        backoff {
+          duration     = "20s"
+          max_duration = "2m"
+          factor       = "2"
+        }
+        limit = "5"
+      }
+    }
+  }
+
+  depends_on = [
+    resource.argocd_application.gateway
+  ]
+}
+
 resource "null_resource" "this" {
-  depends_on = [argocd_application.gateway]
+  depends_on = [
+    argocd_application.gateway,
+    argocd_application.istio_monitoring,
+  ]
 }
